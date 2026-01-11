748ed65b1ac6f6e6f18b757a2649cd673f0a50cf
Stephen O'Connell Clare <128988229+StephenOC12@users.noreply.github.com>
2026-01-11 19:25:41 +0000

    Update test_boundaries.py

--- Tests/test_boundaries.py
+++ Tests/test_boundaries.py
@@ -1,6 +1,7 @@
 from Cards.cards import Card, Suit, Rank
 from Games.play_vs_computer import ComputerPlayer
 
+#Test for how the hard difficulty performs with edge case of card rank
 def test_hard_ai_never_guesses_lower_on_two():
     ai = ComputerPlayer("hard")
 
