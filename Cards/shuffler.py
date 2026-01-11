448f6023b16175a756269aaec53c7656cfb67112
Stephen O'Connell Clare <128988229+StephenOC12@users.noreply.github.com>
2026-01-11 19:17:11 +0000

    Update shuffler.py

--- Cards/shuffler.py
+++ Cards/shuffler.py
@@ -2,19 +2,19 @@ import random
 from typing import List
 from Cards.cards import Card
 
-
+# Abstract base class for shuffling strategies
 class Shuffler:
     def shuffle(self, cards: List[Card]) -> List[Card]:
         raise NotImplementedError
 
-
+#Random shuffler for single player with no seed or vs computer
 class RandomShuffler(Shuffler):
     def shuffle(self, cards: List[Card]) -> List[Card]:
         shuffled = list(cards)
         random.shuffle(shuffled)
         return shuffled
 
-
+#Seeded shuffler for playing with someone else
 class SeededShuffler(Shuffler):
     def __init__(self, seed: int):
         self._rng = random.Random(seed)
